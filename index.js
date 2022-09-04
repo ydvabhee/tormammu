fc2f1ba23f236bfe2473354ac6a8514c3527ebb2
Abhishek Yadav <34765230+ydvabhee@users.noreply.github.com>
2022-09-04 12:37:30 +0000

    added cors

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
+var cors = require("cors");
 const app = require("express")();
+
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.use('/api', require('./routes'));
+app.use("/api", require("./routes"));
 
 app.listen(3000, () => {
   console.log("Listening on port 3000");
